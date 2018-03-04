6e96e13605feab211d69016d28cc9fbe27f7ea00
George Salu <george.salu10@gmail.com>
2018-03-04 19:58:05 -0300

    par nome / valor

---
+++ fundamentos/parNomeValor.js
@@ -0,0 +1,20 @@
+//par nome/valor
+const saudacao = 'Opa'
+
+function exec() {
+    const saudacao = 'Falaaaa'
+    return saudacao
+}
+
+const cliente = {
+    nome : 'Pedro',
+    idade : 32,
+    peso : 90,
+    endereco : {
+        logradouro: 'Rua muito legal',
+        numero : 123
+    } 
+}
+
+console.log(saudacao)
+console.log(exec())
