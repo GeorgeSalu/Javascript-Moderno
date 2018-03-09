a76bdd81f376b4642c3decb17b888a994d2f88b5
George Salu <george.salu10@gmail.com>
2018-03-09 13:20:27 -0300

    introducao a OO

---
+++ objeto/oo.js
@@ -0,0 +1,16 @@
+// codigo nao executavel
+
+// procedural
+processamento(valor1, valor2, valor3)
+
+// OO
+objeto = {
+    valor1, 
+    valor2, 
+    valor3,
+    processamento() {
+        //...
+    }
+}
+
+objeto.processamento()
