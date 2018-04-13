b43bd48fa0ac7f95edef8101f40ca822ea508c2e
George Salu <george.salu10@gmail.com>
2018-04-12 22:05:58 -0300

    concat

---
+++ array/concat.js
@@ -0,0 +1,6 @@
+const filhas = ['ualeska', 'cibalena']
+const filhos = ['uoxiton', 'uesclei']
+const todos = filhas.concat(filhos)
+
+console.log(todos, filhas, filhos)
+console.log([].concat([1,2],[3,4],[6,7], 5))
