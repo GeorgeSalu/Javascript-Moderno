70c9c7ce01882e9dfc0736e5fd59005107caf009
George Salu <george.salu10@gmail.com>
2018-04-17 23:08:16 -0300

    padrao middleware

---
+++ node/Middleware.js
@@ -0,0 +1,11 @@
+// middleware pattern (chain of responsability)
+const passo1 = (ctx, next) => {
+    ctx.valor1 = 'mid1'
+    next()
+}
+
+const passo2 = (ctx, next) => {
+    ctx.valor2 = 'mid2'
+    next()
+}
+
