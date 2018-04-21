735dc898ef8f4bfaee26ad4c9b7196b85fa73ae4
George Salu <george.salu10@gmail.com>
2018-04-21 11:42:29 -0300

    set

---
+++ esnext/set.js
@@ -0,0 +1,12 @@
+
+const times = new Set()
+times.add('vasco')
+times.add('sao paulo').add('palmeiras').add('corinthians')
+times.add('flamengo')
+times.add('vasco')
+
+console.log(times)
+console.log(times.size)
+console.log(times.has('vasco'))
+console.log(times.has('vasco'))
+console.log()
