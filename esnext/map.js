2106cac337ea31e6220b47525a3c165fa18e46af
George Salu <george.salu10@gmail.com>
2018-04-21 11:36:35 -0300

    map

---
+++ esnext/map.js
@@ -0,0 +1,17 @@
+const tecnologias = new Map()
+tecnologias.set('react', { framework: false })
+tecnologias.set('angular', { framework: true })
+
+console.log(tecnologias.get('react').framework)
+
+const chavesVariadas = new Map([
+    [function() {}, 'Funcao'],
+    [{}, 'Objeto'],
+    [123, 'Numero'],
+])
+
+chavesVariadas.forEach((vl, ch) => {
+    console.log(ch, vl)
+})
+
+console.log(chavesVariadas.has(123))
