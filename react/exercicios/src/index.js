0e888d22a2de4fa3dcd4956a965eba9a9d206d20
George Salu <george.salu10@gmail.com>
2018-07-02 14:20:17 -0300

    primeiro componente

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-ReactDom.render(<h1>React</h1>, document.getElementById('root'))
+import Primeiro from './componentes/Primeiro';
+
+ReactDom.render(<Primeiro />, document.getElementById('root'))
