378ec200d528acf015d861313042e506f6ed45c8
George Salu <george.salu10@gmail.com>
2018-07-07 19:09:04 -0300

    componente cadastro Usuario - #03

--- react/crud/frontend/src/main/App.jsx
+++ react/crud/frontend/src/main/App.jsx
@@ -4,7 +4,7 @@ import 'font-awesome/css/font-awesome.min.css'
 import './App.css'
 import React from 'react'
 
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 import Routes from './Routes'
 
@@ -13,11 +13,11 @@ import Nav from '../componentes/template/Nav'
 import Footer from '../componentes/template/Footer'
 
 export default props =>
-    <BrowserRouter>
+    <HashRouter>
         <div className="app">
             <Logo />
             <Nav />
             <Routes />
             <Footer />
         </div>
-    </BrowserRouter>
+    </HashRouter>
