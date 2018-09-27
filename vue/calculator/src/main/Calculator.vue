e53bbb4cd85feb151112566d9ad2de09dbdf2e61
George Salu <george.salu10@gmail.com>
2018-09-27 20:26:03 -0300

    Botoes

--- vue/calculator/src/main/Calculator.vue
+++ vue/calculator/src/main/Calculator.vue
@@ -1,9 +1,23 @@
 <template>
     <div class="calculator">
         <Display />
-        <Button label="AC" triple/>
-        <Button label="/" />
-        <Button label="/" />
+        <Button label="AC" triple />
+        <Button label="/" operation />
+        <Button label="7"  />
+        <Button label="8"  />
+        <Button label="9"  />
+        <Button label="*"  />
+        <Button label="4"  />
+        <Button label="5"  />
+        <Button label="6"  />
+        <Button label="-" operation  />
+        <Button label="1"  />
+        <Button label="2"  />
+        <Button label="3"  />
+        <Button label="+" operation  />
+        <Button label="0"  />
+        <Button label="."  />
+        <Button label="=" operation  />
     </div>
 </template>
 
